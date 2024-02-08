7badab377b653b3413ee9e07e3cefc15e567d0f8
Daniel Santos Martinez <danielsm535@gmail.com>
2024-02-07 23:32:34 -0500

    Initial Commit

---
+++ src/test/java/ds73/cs490backendmilestone2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ds73.cs490backendmilestone2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
